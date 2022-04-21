0d41893117f5ffa08a28801b8886e777ecee53cf
Trent Rand <contact@trentrand.com>
2022-04-20 20:37:23 -0700

    Integrate chendo/marlin-compile-action to build Marlin in CI

---
+++ entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+cd $GITHUB_WORKSPACE
+
+export
+
+if [ -n "$INPUT_CONFIGURATION_URL" ]; then
+  curl "$INPUT_CONFIGURATION_URL" -L > Marlin/Configuration.h
+fi
+
+if [ -n "$INPUT_CONFIGURATION_ADV_URL" ]; then
+  curl "$INPUT_CONFIGURATION_ADV_URL" -L > Marlin/Configuration_adv.h
+fi
+
+platformio run $INPUT_PLATFORMIO_ARGS
+
+ls -alt .pio/build/*/firmware*
